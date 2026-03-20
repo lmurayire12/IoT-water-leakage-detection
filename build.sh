e979f5470020fbc4c8cc0a68edebb7ebb76ca3ec
lmurayire12 <l.murayire@alustudent.com>
2026-03-20 15:50:01 -0400

    Fix build script to install deps in backend folder

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Install Node.js dependencies
-npm install
+# Install Node.js dependencies in the backend folder
+cd backend && npm install && cd ..
 
 # Install Python dependencies for inference.py
 pip install numpy pandas scikit-learn joblib
